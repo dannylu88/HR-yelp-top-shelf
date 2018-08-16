4ee72d51ac48ab09e7f73e5068cecd53c87d82a1
Danny Lu <dannylu8@outlook.com>
2018-08-16 12:16:40 -0700

    update db schema to Cassandra

--- database/business.js
+++ database/business.js
@@ -1,20 +1,25 @@
-
-module.exports = (sequelize, DataTypes) =>{
-  const Business = sequelize.define('business', {
-    restaurant: {type:DataTypes.STRING},
-    rating:{type: DataTypes.INTEGER},
-    price: {type: DataTypes.STRING},
-    category:{type:DataTypes.STRING},
-      street:{type:DataTypes.STRING},
-      city:{type:DataTypes.STRING},
-      state:{type:DataTypes.STRING},
-      zip:{type:DataTypes.STRING},
-      country:{type:DataTypes.STRING},
-  
-    website:{type:DataTypes.STRING},
-    email:{type:DataTypes.STRING},
-    phone:{type:DataTypes.STRING}
-  }, {timestamps:false});
-  return Business;
-};
-
+module.exports = {
+  fields:{
+    id:{
+      type: 'uuid', 
+      default: {'$db_function':'uuid()'}
+    },
+    restaurant:{type:'varchar'},
+    rating:'int',
+    price:'varchar',
+    category:'varchar',
+    street:'varchar',
+    city:'varchar',
+    state:'varchar',
+    zip:'varchar',
+    country:'varchar',
+    website:'varchar',
+    email:'varchar',
+    phone:'varchar',
+    created: {
+      type:'timestamp',
+      default:{'$db_function': 'toTimestamp(now())'}
+    }
+  },
+  key: [['id'], 'created']
+}
