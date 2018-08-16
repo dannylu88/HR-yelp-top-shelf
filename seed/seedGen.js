be586d1851fec1e7b4c26d7e1b77b299b31032fa
Danny Lu <dannylu8@outlook.com>
2018-08-16 16:11:30 -0700

    modified seed.js to generate Cassandra format unique id

--- seed/seedGen.js
+++ seed/seedGen.js
@@ -10,7 +10,7 @@ function seedData(writer, encoding, callback) {
   function write() {
     let ok = true;
     while (i < max && ok) {
-      let id = i;
+      let id = generateUUID();
       let restaurant = faker.lorem.words(3);
       let rating = faker.random.number(5);
       let price = faker.random.number(4);
@@ -23,7 +23,7 @@ function seedData(writer, encoding, callback) {
       let website = faker.internet.url();
       let email = faker.internet.email();
       let phone = faker.phone.phoneNumber();
-      const model = [restaurant, rating, price, category, street, city, state, zip, country, website, email, phone];
+      const model = [id, restaurant, rating, price, category, street, city, state, zip, country, website, email, phone];
       i += 1;
       
       if (i % 1000000 === 0) { 
@@ -51,43 +51,15 @@ function seedData(writer, encoding, callback) {
 seedData(write, 'utf8', () => { console.log('done'); });
 
 
-
-// const faker = require('faker');
-// const fs = require('fs');
-
-// fs.writeFile('data.csv', 'restaurant, rating, price, category, street, city, state, zip, country, website, email, phone', (err) => {  
-//   if (err) throw err;
-//   console.log('Header created!');
-//   let startTime = Date.now()
-//   let recurseSeeding = (count = 0) => {
-    
-//     if(count % 1000000 === 0) {
-//       let timePast = (Date.now() - startTime) / 1000 / 60  //timepast in minutes
-//       console.log(`creating from: ${count + 1} to ${count + 1000000} `);
-//       if(count !== 0) console.log('time to create the above million in mins', timePast);
-//       startTime = Date.now();
-//     }
-//     if(count  === 10000000) return; 
-//       let restaurant = faker.lorem.words(3);
-//       let rating = faker.random.number(5);
-//       let price = faker.random.number(4);
-//       let category = faker.lorem.words(1);
-//       let street = faker.address.streetAddress();
-//       let city = faker.address.city();
-//       let state = faker.address.state();
-//       let zip = faker.address.zipCode();
-//       let country = faker.lorem.words(1);
-//       let website = faker.internet.url();
-//       let email = faker.internet.email();
-//       let phone = faker.phone.phoneNumber();
-      
-    
-    
-//       let entry = '\n' + restaurant + ', ' + rating + ', ' + price + ', ' + category + ', ' + street  + ', ' + city + ', ' + state + ', ' + zip + ', ' + country + ', ' + website + ', ' + email + ', ' + phone; 
-//       fs.appendFile('data.csv', entry, (err) => {
-//         if (err) throw err;
-//         recurseSeeding(count + 1)
-//       });
-//   }
-//   recurseSeeding();
-// });
+function generateUUID() {
+  var d = new Date().getTime();
+  if(Date.now){
+      d = Date.now(); //high-precision timer
+  }
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (d + Math.random()*16)%16 | 0;
+      d = Math.floor(d/16);
+      return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+};
